30588c0aadbacdce0fdd23d3190e27ae377446ef
kartikn89 <34204048+kartikn89@users.noreply.github.com>
2026-01-23 12:33:11 +0530

    Update comment format for app hosting URL

--- project-folder/app.py
+++ project-folder/app.py
@@ -7,9 +7,9 @@ import seaborn as sns
 import os
 import imghdr
 
-"""
-app hosted at: https://curated-by-kartikn89.streamlit.app/ 
-"""
+
+# app hosted at: https://curated-by-kartikn89.streamlit.app/ 
+
 
 from sklearn.metrics import (
     accuracy_score,
